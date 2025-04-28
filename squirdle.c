d1eff6811ffaf4ae245361707116426159e9165b
Mirza Karic <mirza1karic1@gmail.com>
2025-04-28 19:17:54 +0200

    Implement guessing functionality

--- squirdle.c
+++ squirdle.c
@@ -1,14 +1,19 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
 #define WORD_LENGTH 5
 #define WORDS_COUNT 2315
 #define WORDS_FILE "words.txt"
-#define TRIES 5
 #define MAX_INPUT 1000
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 char *rand_squirdle();
 int rand_word_index();
@@ -19,18 +24,37 @@ int main() {
   char *squirdle = rand_squirdle();
 
   printf("squirdle: %s\n", squirdle);
-  guess(squirdle);
+
+  if (guess(squirdle))
+    printf(ANSI_COLOR_GREEN "SUCCESS! YOU GUESSED CORRECTLY" ANSI_COLOR_RESET
+                            "\n");
+  else
+    printf(ANSI_COLOR_RED
+           "OUT OF GUESSES! THE CORRECT WORD WAS: %s" ANSI_COLOR_RESET "\n",
+           squirdle);
+
   free(squirdle);
 
   return 0;
 }
 
 char *rand_squirdle() {
-  int file = open(WORDS_FILE, O_RDONLY);
-  char *squirdle = malloc(WORD_LENGTH * sizeof(char));
-  int offset = rand_word_index();
+  int fd, offset;
+  char *squirdle;
 
-  pread(file, squirdle, 5, offset);
+  fd = open(WORDS_FILE, O_RDONLY);
+  if (fd == -1) {
+    perror("perror");
+    exit(EXIT_FAILURE);
+  }
+
+  squirdle = malloc(WORD_LENGTH * sizeof(char));
+  offset = rand_word_index();
+
+  if ((pread(fd, squirdle, 5, offset)) == -1) {
+    perror("pread");
+    exit(EXIT_FAILURE);
+  }
 
   return squirdle;
 }
@@ -42,11 +66,23 @@ int rand_word_index() {
 }
 
 int guess(char *squirdle) {
-  char *buf[MAX_INPUT];
-  size_t max_input = MAX_INPUT;
-  for (int i = 0; i < TRIES; i++) {
-    getline(buf, &max_input, stdin);
-    printf("guess %d: %s", i + 1, *buf);
+  char *line = NULL;
+  size_t size = 0;
+  int guessed = 0;
+
+  for (int i = 0; i < WORD_LENGTH + 1; i++) {
+    printf("guess %d/%d: ", i + 1, WORD_LENGTH + 1);
+    if (getline(&line, &size, stdin) == -1) {
+      perror("getline");
+      exit(EXIT_FAILURE);
+    }
+
+    if (strncmp(squirdle, line, WORD_LENGTH) == 0) {
+      guessed = 1;
+      break;
+    }
   }
-  return 0;
+
+  free(line);
+  return guessed;
 }
