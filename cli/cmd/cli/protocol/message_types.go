d488b98617cfc44474918ed0f8d03d968c6c1c2a
Mirza Karic <mirza1karic1@gmail.com>
2025-12-04 18:22:41 +0100

    Add MessageType constants

--- cli/cmd/cli/protocol/message_types.go
+++ cli/cmd/cli/protocol/message_types.go
@@ -1,88 +1,108 @@
 package protocol
 
-import (
-	"encoding/json"
-	"log"
+type MessageType string
+
+const (
+	CREATE_MATCH        MessageType = "CREATE_MATCH"
+	JOIN_ROOM           MessageType = "JOIN_ROOM"
+	CREATE_ROOM         MessageType = "CREATE_ROOM"
+	MAKE_GUESS          MessageType = "MAKE_GUESS"
+	REQUEST_REMATCH     MessageType = "REQUEST_REMATCH"
+	LEAVE_MATCH         MessageType = "LEAVE_MATCH"
+	ERROR               MessageType = "ERROR"
+	CONNECTED           MessageType = "CONNECTED"
+	ROOM_CREATED        MessageType = "ROOM_CREATED"
+	ROOM_JOINED         MessageType = "ROOM_JOINED"
+	ROOM_JOIN_FAILED    MessageType = "ROOM_JOIN_FAILED"
+	WAIT_OPPONENT_JOIN  MessageType = "WAIT_OPPONENT_JOIN"
+	MATCH_STARTED       MessageType = "MATCH_STARTED"
+	ROUND_STARTED       MessageType = "ROUND_STARTED"
+	WAIT_GUESS          MessageType = "WAIT_GUESS"
+	WAIT_OPPONENT_GUESS MessageType = "WAIT_OPPONENT_GUESS"
+	GUESS_RESULT        MessageType = "GUESS_RESULT"
+	ROUND_FINISHED      MessageType = "ROUND_FINISHED"
+	MATCH_FINISHED      MessageType = "MATCH_FINISHED"
+	BYE                 MessageType = "BYE"
 )
 
 type EnvelopeMessage struct {
-	Type string `json:"type"`
+	Type MessageType `json:"type"`
 }
 
 /* Client Types */
 
 type CreateMatchMessage struct {
-	Type       string   `json:"type"`
-	Mode       GameMode `json:"mode"`
-	Rounds     int      `json:"rounds"`
-	WordLength int      `json:"wordLength"`
+	Type    MessageType `json:"type"`
+	Mode    GameMode    `json:"mode"`
+	WordLen int         `json:"wordLength"`
+	Rounds  int         `json:"rounds"`
+}
+
+func NewCreateMatchMessage(mode GameMode, wordLen int, rounds int) *CreateMatchMessage {
+	return &CreateMatchMessage{
+		Type:    CREATE_MATCH,
+		WordLen: wordLen,
+		Rounds:  rounds,
+	}
 }
 
 type MakeGuessMessage struct {
-	Type  string `json:"type"`
-	Guess string `json:"guess"`
+	Type  MessageType `json:"type"`
+	Guess string      `json:"guess"`
 }
 
 func NewMakeGuessMessage(guess string) *MakeGuessMessage {
 	return &MakeGuessMessage{
-		Type:  "MAKE_GUESS",
+		Type:  MAKE_GUESS,
 		Guess: guess,
 	}
 }
 
-func (m *MakeGuessMessage) JSON() string {
-	if v, err := json.Marshal(m); err != nil {
-		log.Print("[MakeGuessMessage.JSON] Failed to marshal")
-		return ""
-	} else {
-		return string(v)
-	}
-}
-
 type LeaveMatchMessage struct {
-	EnvelopeMessage
+	Type MessageType `json:"type"`
 }
 
 /* Server Types */
 
 type ErrorMessage struct {
-	Type   string `json:"type"`
-	Reason string `json:"reason"`
+	Type   MessageType `json:"type"`
+	Reason string      `json:"reason"`
 }
 
 type MatchStartedMessage struct {
-	Type       string `json:"type"`
-	MatchID    string `json:"matchId"`
-	Rounds     int    `json:"rounds"`
-	WordLength int    `json:"wordLength"`
+	Type       MessageType `json:"type"`
+	MatchID    string      `json:"matchId"`
+	Rounds     int         `json:"rounds"`
+	WordLength int         `json:"wordLength"`
 }
 
 type RoundStartedMessage struct {
-	Type        string `json:"type"`
-	RoundNumber int    `json:"roundNumber"`
+	Type        MessageType `json:"type"`
+	RoundNumber int         `json:"roundNumber"`
 }
 
 type WaitGuessMessage struct {
-	Type string `json:"type"`
+	Type MessageType `json:"type"`
 }
 
 type WaitOpponentGuessMessage struct {
-	Type string `json:"type"`
+	Type MessageType `json:"type"`
 }
 
 type GuessResultMessage struct {
-	Type     string           `json:"type"`
+	Type     MessageType      `json:"type"`
 	Success  bool             `json:"success"`
+	Guess    string           `json:"guess"`
 	Feedback []LetterFeedback `json:"feedback"`
 }
 
 type RoundFinishedMessage struct {
-	Type    string `json:"type"`
-	Success bool   `json:"success"`
-	Word    string `json:"word"`
+	Type    MessageType `json:"type"`
+	Success bool        `json:"success"`
+	Word    string      `json:"word"`
 }
 
 type MatchFinishedMessage struct {
-	Type   string `json:"type"`
-	Winner string `json:"winner"`
+	Type   MessageType `json:"type"`
+	Winner string      `json:"winner"`
 }
