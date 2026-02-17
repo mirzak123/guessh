4dbc509adbfa624c11509f13383a388b9b26adb0
Mirza Karic <mirza1karic1@gmail.com>
2026-02-17 18:40:27 +0100

    Clean up form

--- cli/internal/screen/start_menu.go
+++ cli/internal/screen/start_menu.go
@@ -26,138 +26,143 @@ var GameModeLabels = map[protocol.GameMode]string{
 func NewStartMenu(matchInfo *game.MatchInfo) (*huh.Form, *bool) {
 	confirm := true
 
-	form := huh.NewForm(
-		huh.NewGroup(
-			huh.NewSelect[protocol.GameMode]().
+	var (
+		playerNameInput = huh.NewInput().
+				Title("Player name ").
+				Value(&matchInfo.PlayerName).
+				Validate(func(str string) error {
+				if matchInfo.PlayerName == "" {
+					return errors.New("name must not be empty")
+				}
+				return nil
+			})
+
+		modeInput = huh.NewSelect[protocol.GameMode]().
 				Title("Are you lonely?").
 				Options(
-					huh.NewOption(GameModeLabels[protocol.SINGLE], protocol.SINGLE),
-					huh.NewOption(GameModeLabels[protocol.MULTI_REMOTE], protocol.MULTI_REMOTE),
-				).
-				Value(&matchInfo.Mode),
-		),
+				huh.NewOption(GameModeLabels[protocol.SINGLE], protocol.SINGLE),
+				huh.NewOption(GameModeLabels[protocol.MULTI_REMOTE], protocol.MULTI_REMOTE),
+			).
+			Value(&matchInfo.Mode)
 
-		huh.NewGroup(
-			huh.NewSelect[bool]().
-				Title("Create or join?").
-				Options(
-					huh.NewOption("Create new match", false),
-					huh.NewOption("Join existing match", true),
-				).
-				Value(&matchInfo.JoinExisting),
-		).WithHideFunc(func() bool {
-			return matchInfo.Mode != protocol.MULTI_REMOTE
-		}),
+		joinExistingInput = huh.NewSelect[bool]().
+					Title("Create or join?").
+					Options(
+				huh.NewOption("Create new match", false),
+				huh.NewOption("Join existing match", true),
+			).
+			Value(&matchInfo.JoinExisting)
 
-		huh.NewGroup(
-			huh.NewSelect[int]().
+		wordLenInput = huh.NewSelect[int]().
 				Title("How many letters?").
 				Options(
-					huh.NewOption("Five", 5),
-					huh.NewOption("Six", 6),
-					huh.NewOption("Seven", 7),
-				).
-				Value(&matchInfo.WordLen),
+				huh.NewOption("Five", 5),
+				huh.NewOption("Six", 6),
+				huh.NewOption("Seven", 7),
+			).
+			Value(&matchInfo.WordLen)
 
-			huh.NewInput().
+		roundNumInput = huh.NewInput().
 				Title(fmt.Sprintf("How many rounds? (%d - %d)", minRounds, maxRounds)).
 				Validate(func(str string) error {
-					r, err := strconv.Atoi(str)
-					if err != nil {
-						return errors.New("please enter a valid number")
+				r, err := strconv.Atoi(str)
+				if err != nil {
+					return errors.New("please enter a valid number")
+				}
+				if r < minRounds || r > maxRounds {
+					return fmt.Errorf("round number must be between %d and %d", minRounds, maxRounds)
+				}
+				return nil
+			}).
+			Value(&matchInfo.RawTotalRounds)
+
+		roomIDInput = huh.NewInput().
+				Title("Room ID ").
+				Value(&matchInfo.RoomID).
+				Description("Enter the Room ID shared with you").
+				Validate(func(str string) error {
+				matchInfo.RoomID = strings.ToUpper(str)
+
+				if str != "" && len(str) != protocol.ROOM_ID_LENGTH {
+					return fmt.Errorf("room ID must be %d characters long", protocol.ROOM_ID_LENGTH)
+				}
+				return nil
+			}).
+			CharLimit(protocol.ROOM_ID_LENGTH)
+
+		cornfirmInput = huh.NewConfirm().
+				TitleFunc(func() string {
+				if matchInfo.Mode == protocol.MULTI_REMOTE {
+					if matchInfo.RoomID != "" {
+						return fmt.Sprintf("Join room %s ?", lipgloss.NewStyle().Foreground(lipgloss.Color(ui.Gray)).Render(matchInfo.RoomID))
+					} else {
+						return "Create new room?"
 					}
-					if r < minRounds || r > maxRounds {
-						return fmt.Errorf("round number must be between %d and %d", minRounds, maxRounds)
+				} else {
+					return "Start match?"
+				}
+			}, nil).
+			Value(&confirm).
+			WithButtonAlignment(lipgloss.Left)
+
+		summaryNote = huh.NewNote().
+				Title("Summary").
+				DescriptionFunc(func() string {
+				labelStyle := lipgloss.NewStyle().Bold(true)
+				valueStyle := lipgloss.NewStyle().Foreground(lipgloss.Color(ui.Gray))
+
+				line := func(label, value string) string {
+					return labelStyle.Render(label) + valueStyle.Render(value)
+				}
+
+				var lines []string
+				lines = append(lines, line("Mode: ", GameModeLabels[matchInfo.Mode]))
+				lines = append(lines, line("Word length: ", fmt.Sprintf("%d", matchInfo.WordLen)))
+				lines = append(lines, line("Rounds: ", matchInfo.RawTotalRounds))
+
+				if matchInfo.Mode == protocol.MULTI_REMOTE {
+					lines = append(lines, line("Player name: ", matchInfo.PlayerName))
+					if matchInfo.RoomID != "" {
+						lines = append(lines, line("Room ID: ", matchInfo.RoomID))
 					}
-					return nil
-				}).
-				Value(&matchInfo.RawTotalRounds),
+				}
+
+				return lipgloss.JoinVertical(lipgloss.Left, lines...)
+
+			}, &matchInfo.RoomID).Height(10) // BUG: Rerender this if other fields change, not only RoomID
+	)
+
+	form := huh.NewForm(
+		huh.NewGroup(modeInput),
+
+		huh.NewGroup(joinExistingInput).
+			WithHideFunc(func() bool {
+				return matchInfo.Mode != protocol.MULTI_REMOTE
+			}),
+
+		huh.NewGroup(
+			wordLenInput,
+			roundNumInput,
 		).WithHideFunc(func() bool {
 			return matchInfo.JoinExisting
 		}),
 
 		huh.NewGroup(
-			huh.NewInput().
-				Title("Player name ").
-				Value(&matchInfo.PlayerName).
-				Validate(func(str string) error {
-					if matchInfo.PlayerName == "" {
-						return errors.New("name must not be empty")
-					}
-					return nil
-				}),
-			huh.NewInput().
-				Title("Room ID ").
-				Value(&matchInfo.RoomID).
-				Description("Enter the Room ID shared with you").
-				Validate(func(str string) error {
-					matchInfo.RoomID = strings.ToUpper(str)
-
-					if str != "" && len(str) != protocol.ROOM_ID_LENGTH {
-						return fmt.Errorf("room ID must be %d characters long", protocol.ROOM_ID_LENGTH)
-					}
-					return nil
-				}).
-				CharLimit(protocol.ROOM_ID_LENGTH),
+			playerNameInput,
+			roomIDInput,
 		).WithHideFunc(func() bool {
 			return matchInfo.Mode != protocol.MULTI_REMOTE || !matchInfo.JoinExisting
 		}),
 
 		huh.NewGroup(
-			huh.NewInput().
-				Title("Player name ").
-				Value(&matchInfo.PlayerName).
-				Validate(func(str string) error {
-					if matchInfo.PlayerName == "" {
-						return errors.New("name must not be empty")
-					}
-					return nil
-				}),
+			playerNameInput,
 		).WithHideFunc(func() bool {
 			return matchInfo.Mode != protocol.MULTI_REMOTE || matchInfo.JoinExisting
 		}),
 
 		huh.NewGroup(
-			huh.NewConfirm().
-				TitleFunc(func() string {
-					if matchInfo.Mode == protocol.MULTI_REMOTE {
-						if matchInfo.RoomID != "" {
-							return fmt.Sprintf("Join room %s ?", lipgloss.NewStyle().Foreground(lipgloss.Color(ui.Gray)).Render(matchInfo.RoomID))
-						} else {
-							return "Create new room?"
-						}
-					} else {
-						return "Start match?"
-					}
-				}, nil).
-				Value(&confirm).
-				WithButtonAlignment(lipgloss.Left),
-
-			huh.NewNote().
-				Title("Summary").
-				DescriptionFunc(func() string {
-					labelStyle := lipgloss.NewStyle().Bold(true)
-					valueStyle := lipgloss.NewStyle().Foreground(lipgloss.Color(ui.Gray))
-
-					line := func(label, value string) string {
-						return labelStyle.Render(label) + valueStyle.Render(value)
-					}
-
-					var lines []string
-					lines = append(lines, line("Mode: ", GameModeLabels[matchInfo.Mode]))
-					lines = append(lines, line("Word length: ", fmt.Sprintf("%d", matchInfo.WordLen)))
-					lines = append(lines, line("Rounds: ", matchInfo.RawTotalRounds))
-
-					if matchInfo.Mode == protocol.MULTI_REMOTE {
-						lines = append(lines, line("Player name: ", matchInfo.PlayerName))
-						if matchInfo.RoomID != "" {
-							lines = append(lines, line("Room ID: ", matchInfo.RoomID))
-						}
-					}
-
-					return lipgloss.JoinVertical(lipgloss.Left, lines...)
-
-				}, &matchInfo.RoomID).Height(10), // BUG: Rerender this if other fields change, not only RoomID
+			cornfirmInput,
+			summaryNote,
 		),
 	)
 
